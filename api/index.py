8ae74d258793f122e414e1e6f3ce7cd9621e77ae
Evan <uyevan@163.com>
2024-06-07 20:18:34 +0800

    :bug: 修复刷新404问题.

--- api/index.py
+++ api/index.py
@@ -11,3 +11,23 @@ def indexA():
 @app.route('/index', methods=['GET'])
 def indexB():
     return render_template("index.html")
+
+
+@app.route('/api-dock-v1.html', methods=['GET'])
+def indexC():
+    return render_template('api-dock-v1.html')
+
+
+@app.route('/api-dock-v2.html', methods=['GET'])
+def indexD():
+    return render_template('api-dock-v2.html')
+
+
+@app.route('/api-about.html', methods=['GET'])
+def indexF():
+    return render_template('api-about.html')
+
+
+@app.route('/404.html', methods=['GET'])
+def index404():
+    return render_template('404.html')
